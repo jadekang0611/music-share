18dcc219615e94955f5b8462dc6d851cb3d8d91d
Jade <56938630+jadekang0611@users.noreply.github.com>
2022-11-20 02:17:21 -0800

    Refactored Apollo Client to execute subscription and normal HTTP used queries depending on the definition result using the split

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,12 +1,44 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: 'https://on-firefly-85.hasura.app/v1/graphql',
-  cache: new InMemoryCache(),
   headers: {
     'x-hasura-admin-secret':
       'LzODuRqK4UV61gr0O1nULEu0OIWvnRC9QDWeaCK5oXEnZp4ZdtZJKh86UBIkDRBY',
   },
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://on-firefly-85.hasura.app/v1/graphql',
+  })
+);
+
+// export default client;
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
 });
 
 export default client;
